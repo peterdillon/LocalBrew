a681bf827de29cc62fb70fcf9df9cf3cc9b40426
Peter Dillon <fingermaster@gmail.com>
2021-10-15 14:07:55 -0400

    add active route class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Spinkit } from 'ng-http-loader';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 
@@ -7,7 +8,11 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  constructor( public router: Router ) {}
+  
   spinnerStyle = Spinkit;
   public spinnerComponent = SpinnerComponent;
 }
